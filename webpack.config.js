9630809d18478946614bb8dc2890e7a1ea84bda9
jmoseka <jamylam3@gmail.com>
2022-05-22 17:50:12 +0300

    Add card styling

--- webpack.config.js
+++ webpack.config.js
@@ -33,6 +33,7 @@ module.exports = {
           options: {
             limit: 8000, // Convert images < 8kb to base64 strings
             name: 'images/[hash]-[name].[ext]',
+            output: 'images',
           },
         }],
       },
